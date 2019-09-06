93d9849935f551be12b90c2bdb248407a578ca3b
emerald21 <aditi.jain@cumminscollege.in>
2019-09-06 10:21:27 +0530

    Add files via upload

--- Citi_Project/Sample/src/com/pojo/User.java
+++ Citi_Project/Sample/src/com/pojo/User.java
@@ -2,9 +2,16 @@ package com.pojo;
 
 public class User {
 
+	protected int userId;
 	protected String username;
 	protected String password;
-	public int UserId;
-	protected String phnNo;
+	protected String firstName;
+	protected String lastName;
+	protected String mailId;
+	protected String country;
+	protected String phoneNo;
+	protected int accountNo_USD;
+	protected int accountNo_EUR;
+	protected int accountNo_GBP;
 	
 }
